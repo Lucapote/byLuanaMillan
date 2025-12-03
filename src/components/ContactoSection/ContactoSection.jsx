7e22217f5452fdae92c7458c340898d07aeab5ae
ASUS <ASUS@DESKTOP-3CFN9QQ>
2025-12-02 21:48:44 -0500

    metemos cambios responsives

--- src/components/ContactoSection/ContactoSection.jsx
+++ src/components/ContactoSection/ContactoSection.jsx
@@ -1,24 +1,44 @@
 import { FormularioContacto } from '../FormularioContacto/FormularioContacto'
 import { TextoDestacado } from '../TextoDestacado/TextoDestacado'
+import { useMediaQuery } from 'react-responsive'
 import styles from './ContactoSection.module.css'
 
 export const ContactoSection = () => {
+    const isMobile = useMediaQuery({maxWidth: 767})
   return (
     <section id="contacto" className={styles.contactoSection}>
         <div className={styles.contenedorPrincipal}>
-            <div className={styles.contenedorIzquierda}>
+        {isMobile ? (
+                        <>
                 <div className={styles.contenedorTextos}>
                     <TextoDestacado as='h6'>Contacto</TextoDestacado>
                     <h5 className={styles.tituloContacto}>Hablemos de tu idea</h5>
                     <p className={styles.textoContacto}>Si querés darle vida a tu marca con un diseño pensado para vos, escribime.</p>
-                </div>
+                </div>  
+
+                <FormularioContacto/>
 
                 <img src="/img/contacto.png" alt="lumiContacto" className={styles.lumiContacto}/>
-            </div>
+            </>
+        ):
+        (
+            <>
+                <div className={styles.contenedorIzquierda}>
+                    <div className={styles.contenedorTextos}>
+                        <TextoDestacado as='h6'>Contacto</TextoDestacado>
+                        <h5 className={styles.tituloContacto}>Hablemos de tu idea</h5>
+                        <p className={styles.textoContacto}>Si querés darle vida a tu marca con un diseño pensado para vos, escribime.</p>
+                    </div>
 
-            <div className={styles.contenedorDerecha}>
-                <FormularioContacto/>
-            </div>
+                    <img src="/img/contacto.png" alt="lumiContacto" className={styles.lumiContacto}/>
+                </div>
+
+                <div className={styles.contenedorDerecha}>
+                    <FormularioContacto/>
+                </div>
+            </>
+        )}
+
         </div>
     </section>
   )
